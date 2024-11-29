f2ce5b34be519e9cb75a3a3ba8cd7813a95cc06a
Piero <pieroluchi@hotmail.com>
2024-11-29 11:46:07 +0100

    test1

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template,request
 
 app=Flask(__name__)
 
-@app.route("/")
-def homepage():
-    return "<h1 style='color:red'>Hello World!</h1>"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-
-@app.route("/contatti")
-def contatti():
-    return "Contattaci!"
-
+@app.route('/api', methods=['POST'])
+def api():
+    # This function will be called when the button is pressed
+    data = request.form['data']  # Retrieve data from the form
+    
+    print(f"Received data: {data}")  # Process the data as needed
+    return "Data received!"
 
 if __name__ == '__main__':
     app.run(debug=True)
