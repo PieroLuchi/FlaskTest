8465a66d8d4cd370b048e187e35005ce9ef1e559
Piero <pieroluchi@hotmail.com>
2024-11-29 11:05:12 +0100

    first

---
+++ Helloword.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return "<h1 style='color:red'>Hello World!</h1>"
+
+
+@app.route("/contatti")
+def contatti():
+    return "Contattaci!"
